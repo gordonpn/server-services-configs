38c3ba50e1e55c4044d449eb8521fbf91a49f5f0
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-05-13 11:51:15 -0400

    fix: specify where the docker-compose binary is
    crontab did not find the command otherwise

--- scripts/update_containers.sh
+++ scripts/update_containers.sh
@@ -18,8 +18,8 @@ container_dirs=(
 for dir in "${container_dirs[@]}"
 do
 	cd "$base_path"/"$dir" || break
-	docker-compose pull
-	docker-compose up --remove-orphans --detach
+	/usr/local/bin/docker-compose pull
+	/usr/local/bin/docker-compose up --remove-orphans --detach
 done
 
 curl --retry 3 https://hc-ping.com/"$UPDATE_HC_UUID"
