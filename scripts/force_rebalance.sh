56df45b58473067a45ade0a7ca6d662a3064b4a6
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-07-10 09:31:00 -0400

    chore: check for node availability before performing anything

--- scripts/force_rebalance.sh
+++ scripts/force_rebalance.sh
@@ -21,8 +21,11 @@ abs() {
     [[ $(( $@ )) -lt 0 ]] && echo "$(( ($@) * -1 ))" || echo "$(( $@ ))"
 }
 
-mapfile -t server_tasks < <( /usr/bin/docker node ps --filter desired-state=running gordonpn-pi --format "{{.Name}}" )
-mapfile -t pi_tasks < <( /usr/bin/docker node ps --filter desired-state=running gordonpn-server --format "{{.Name}}" )
+mapfile -t pi_tasks < <( /usr/bin/docker node ps --filter desired-state=running gordonpn-pi --format "{{.Name}}" )
+mapfile -t server_tasks < <( /usr/bin/docker node ps --filter desired-state=running gordonpn-server --format "{{.Name}}" )
+
+pi_availability=$(/usr/bin/docker node inspect gordonpn-pi --format "{{ .Spec.Availability }}")
+server_availability=$(/usr/bin/docker node inspect gordonpn-server --format "{{ .Spec.Availability }}")
 
 num_server_tasks=${#server_tasks[@]}
 num_pi_tasks=${#pi_tasks[@]}
@@ -32,25 +35,31 @@ difference=$(abs "$num_server_tasks" - "$num_pi_tasks")
 printf 'total tasks running: %d\n' "$total_tasks"
 printf '%d tasks running on the server\n' "$num_server_tasks"
 printf '%d tasks running on the pi\n' "$num_pi_tasks"
-printf 'difference: %d\n' "$difference"
+printf 'difference of tasks: %d\n' "$difference"
 
-if [ "$difference" -gt $(( total_tasks / 2 )) ]; then
-	printf 'rotating certificates...\n'
-	/usr/bin/docker swarm ca --rotate --quiet
-	sleep 10
+if [ "$pi_availability" == "active" ] && [ "$server_availability" == "active" ]; then
+	if [ "$difference" -gt $(( total_tasks / 2 )) ]; then
+		printf 'rotating certificates...\n'
+		/usr/bin/docker swarm ca --rotate --quiet
+		sleep 10
 
-	mapfile -t services_array < <( /usr/bin/docker service ls --format "{{.Name}}" )
+		mapfile -t services_array < <( /usr/bin/docker service ls --format "{{.Name}}" )
 
-	for service in "${services_array[@]}"; do
-		if [ "$service" == "traefik_traefik" ]; then
-			continue
-		fi
-		printf 'force rebalancing service: %s\n' "$service"
-		/usr/bin/docker service update --force --quiet "$service" &
-		sleep 2
-	done
+		for service in "${services_array[@]}"; do
+			if [ "$service" == "traefik_traefik" ]; then
+				continue
+			fi
+			printf 'force rebalancing service: %s\n' "$service"
+			/usr/bin/docker service update --force --quiet "$service" &
+			sleep 2
+		done
+
+	else
+		printf 'not rebalancing\n'
+	fi
 else
-	printf 'not rebalancing\n'
+	printf 'either Raspberry Pi or Server not available'
+	printf 'nothing performed'
 fi
 
 clean_log
