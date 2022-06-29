163dec7bfdb8a0b9a8865121c21c9a786225bce6
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2022-06-29 00:48:07 -0400

    ci(fix): add spec version back into processed compose file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,20 +7,8 @@ if [ "$INSIDE_DOCKER" != "true" ]; then
 	source "$scripts_dir"/.env || exit 1
 	set +o allexport
 	source "$scripts_dir"/trim_logs.sh
-	docker_compose_command() {
-		/usr/local/bin/docker-compose "$1"
-	}
-	docker_command() {
-		/usr/bin/docker "$1"
-	}
 else
 	echo 'Running inside a Docker container'
-	docker_compose_command() {
-		docker compose "$1"
-	}
-	docker_command() {
-		docker "$1"
-	}
 fi
 
 curl -s --retry 3 https://hc-ping.com/"$UPDATE_HC_UUID"/start
@@ -42,6 +30,7 @@ for service in "${services[@]}"; do
 	printf "\n=========== START: %s =========== SERVICE: %s\n" "$(date +%F_%T)" "$service"
 	cd "$scripts_dir"/../"$service"/ || break
 	docker compose convert >|docker-compose.processed.yml
+	echo -e "version: \"3.8\"\n$(cat docker-compose.processed.yml)" >|docker-compose.processed.yml
 	docker stack deploy -c docker-compose.processed.yml "$service"
 	printf "\n=========== END : %s ==========================\n" "$(date +%F_%T)"
 done
